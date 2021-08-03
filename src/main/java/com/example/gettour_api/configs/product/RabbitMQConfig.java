b3e11e9621f0737c748fd7ce6b535cfc599ad9e6
shafigtahmasib <shafigtahmasib@gmail.com>
2021-08-03 13:52:04 +0400

    Refactoring

--- src/main/java/com/example/gettour_api/configs/product/RabbitMQConfig.java
+++ src/main/java/com/example/gettour_api/configs/product/RabbitMQConfig.java
@@ -1,8 +1,8 @@
-package com.example.gettour_api.configs;
+package com.example.gettour_api.configs.product;
 
-import com.example.gettour_api.utils.Config;
 import lombok.Data;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -12,8 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 
-@Profile("dev")
+@Profile("!dev")
 @Data
 @Configuration
 public class RabbitMQConfig {
@@ -55,9 +57,17 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public AmqpTemplate template(ConnectionFactory connectionFactory){
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(converter());
-        return rabbitTemplate;
+    public ConnectionFactory connectionFactory() throws URISyntaxException {
+        final URI rabbitMqUrl = new URI(System.getenv("CLOUDAMQP_URL"));
+        final CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setUri(rabbitMqUrl);
+        return factory;
+    }
+
+    @Bean
+    public RabbitTemplate template() throws URISyntaxException {
+        RabbitTemplate temp = new RabbitTemplate(connectionFactory());
+        temp.setMessageConverter(converter());
+        return temp;
     }
 }
